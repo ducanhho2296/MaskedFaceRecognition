144b78dfef0100bf4dcdf68d770c94d3d30476db
Duc Anh Ho <92146886+ducanhho2296@users.noreply.github.com>
2022-06-26 16:19:33 +0200

    upload face recognition core
    demo version for face recognition with Resnet34 and Local Binary Pattern

---
+++ Face recognition/arcface_model.py
@@ -0,0 +1,67 @@
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.python.keras.engine import training
+import numpy as np
+
+def ResNet34():
+
+	img_input = layers.Input(shape=(112, 112, 3))
+
+	x = layers.ZeroPadding2D(padding=1, name='conv1_pad')(img_input)
+	x = layers.Conv2D(64, 3, strides=1, use_bias=False, 
+                   kernel_initializer='glorot_normal', 
+                   name='conv1_conv')(x)
+	x = layers.BatchNormalization(axis=3, epsilon=2e-5, momentum=0.9, name='conv1_bn')(x)
+	x = layers.PReLU(shared_axes=[1, 2], name='conv1_prelu')(x)
+	x = stack_fn(x)
+
+	model = training.Model(img_input, x, name='ResNet34')
+
+	return model
+
+def block1(x, filters, kernel_size=3, stride=1, conv_shortcut=True, name=None):
+	bn_axis = 3
+
+	if conv_shortcut:
+		shortcut = layers.Conv2D(filters, 1, strides=stride, use_bias=False, kernel_initializer='glorot_normal', name=name + '_0_conv')(x)
+		shortcut = layers.BatchNormalization(axis=bn_axis, epsilon=2e-5, momentum=0.9, name=name + '_0_bn')(shortcut)
+	else:
+		shortcut = x
+
+	x = layers.BatchNormalization(axis=bn_axis, epsilon=2e-5, momentum=0.9, name=name + '_1_bn')(x)
+	x = layers.ZeroPadding2D(padding=1, name=name + '_1_pad')(x)
+	x = layers.Conv2D(filters, 3, strides=1, kernel_initializer='glorot_normal', use_bias=False, name=name + '_1_conv')(x)
+	x = layers.BatchNormalization(axis=bn_axis, epsilon=2e-5, momentum=0.9, name=name + '_2_bn')(x)
+	x = layers.PReLU(shared_axes=[1, 2], name=name + '_1_prelu')(x)
+
+	x = layers.ZeroPadding2D(padding=1, name=name + '_2_pad')(x)
+	x = layers.Conv2D(filters, kernel_size, strides=stride, kernel_initializer='glorot_normal', use_bias=False, name=name + '_2_conv')(x)
+	x = layers.BatchNormalization(axis=bn_axis, epsilon=2e-5, momentum=0.9, name=name + '_3_bn')(x)
+
+	x = layers.Add(name=name + '_add')([shortcut, x])
+	return x
+
+def stack1(x, filters, blocks, stride1=2, name=None):
+	x = block1(x, filters, stride=stride1, name=name + '_block1')
+	for i in range(2, blocks + 1):
+		x = block1(x, filters, conv_shortcut=False, name=name + '_block' + str(i))
+	return x
+
+def stack_fn(x):
+	x = stack1(x, 64, 3, name='conv2')
+	x = stack1(x, 128, 4, name='conv3')
+	x = stack1(x, 256, 6, name='conv4')
+	return stack1(x, 512, 3, name='conv5')
+
+def findCosineDistance(source_representation, test_representation):
+    a = np.matmul(np.transpose(source_representation), test_representation)
+    b = np.sum(np.multiply(source_representation, source_representation))
+    c = np.sum(np.multiply(test_representation, test_representation))
+    dist = 1 - (a / (np.sqrt(b) * np.sqrt(c)))
+    print("cosine similarity: ", dist)
+    #similarity threshold
+    if dist >= 0 and dist < 0.35: print("Same person")
+    elif dist >= 0.35 and dist < 0.7: print("Must use LBP to verify this pair")
+    else: print("not the same person")
+    return dist
