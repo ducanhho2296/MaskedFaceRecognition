38fc634916cdaa33dcce84f382aef17ed7356f9e
ducanhho2296 <ducanh.ho2296@gmail.com>
2022-07-03 12:18:26 +0200

    update ROI

--- MFR_challenge/Face_recognition/ROI_extraction.py
+++ MFR_challenge/Face_recognition/ROI_extraction.py
@@ -99,44 +99,44 @@ def extract_ROI(img_path, shape_predictor_68_face_landmarks_path):
 
  #------- encode ROI of faces in LBP and plot histogram-------------
 def calculate_LBP(img_path):    
-  img = cv2.imread(img_path)
-  def assign_bit(image, x, y, c):   #comparing bit with threshold value of centre pixel
-      bit = 0  
-      try:          
-          if image[x][y] >= c: 
-              bit = 1         
-      except: 
-          pass
-      return bit 
-      
-  def local_bin_val(image, x, y):  #calculating local binary pattern value of a pixel
-      eight_bit_binary = []
-      centre = image[x][y] 
-      powers = [1, 2, 4, 8, 16, 32, 64, 128] 
-      decimal_val = 0
-      #starting from top right,assigning bit to pixels clockwise 
-      eight_bit_binary.append(assign_bit(image, x-1, y + 1,centre)) 
-      eight_bit_binary.append(assign_bit(image, x, y + 1, centre)) 
-      eight_bit_binary.append(assign_bit(image, x + 1, y + 1, centre)) 
-      eight_bit_binary.append(assign_bit(image, x + 1, y, centre)) 
-      eight_bit_binary.append(assign_bit(image, x + 1, y-1, centre)) 
-      eight_bit_binary.append(assign_bit(image, x, y-1, centre)) 
-      eight_bit_binary.append(assign_bit(image, x-1, y-1, centre)) 
-      eight_bit_binary.append(assign_bit(image, x-1, y, centre))     
-      #calculating decimal value of the 8-bit binary number
-      for i in range(len(eight_bit_binary)): 
-          decimal_val += eight_bit_binary[i] * powers[i] 
+    img = cv2.imread(img_path)
+    def assign_bit(image, x, y, c):   #comparing bit with threshold value of centre pixel
+        bit = 0  
+        try:          
+            if image[x][y] >= c: 
+                bit = 1         
+        except: 
+            pass
+        return bit 
+        
+    def local_bin_val(image, x, y):  #calculating local binary pattern value of a pixel
+        eight_bit_binary = []
+        centre = image[x][y] 
+        powers = [1, 2, 4, 8, 16, 32, 64, 128] 
+        decimal_val = 0
+        #starting from top right,assigning bit to pixels clockwise 
+        eight_bit_binary.append(assign_bit(image, x-1, y + 1,centre)) 
+        eight_bit_binary.append(assign_bit(image, x, y + 1, centre)) 
+        eight_bit_binary.append(assign_bit(image, x + 1, y + 1, centre)) 
+        eight_bit_binary.append(assign_bit(image, x + 1, y, centre)) 
+        eight_bit_binary.append(assign_bit(image, x + 1, y-1, centre)) 
+        eight_bit_binary.append(assign_bit(image, x, y-1, centre)) 
+        eight_bit_binary.append(assign_bit(image, x-1, y-1, centre)) 
+        eight_bit_binary.append(assign_bit(image, x-1, y, centre))     
+        #calculating decimal value of the 8-bit binary number
+        for i in range(len(eight_bit_binary)): 
+            decimal_val += eight_bit_binary[i] * powers[i] 
             
-      return decimal_val 
-  m, n, _ = img.shape 
-  gray_scale = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  #converting image to grayscale
-  lbp_img = np.zeros((m, n),np.uint8) 
-  # converting image to lbp
-  for i in range(0,m): 
-      for j in range(0,n): 
-          lbp_img[i, j] = local_bin_val(gray_scale, i, j) 
- 
-  n_bins1 = int(lbp_img.max() + 1)
-  hist, _ = np.histogram(lbp_img, density=True, bins=n_bins1, range=(0, n_bins1))   #get the histogram for distance calculation task
-  return hist, lbp_img
+        return decimal_val 
+    m, n, _ = img.shape 
+    gray_scale = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  #converting image to grayscale
+    lbp_img = np.zeros((m, n),np.uint8) 
+    # converting image to lbp
+    for i in range(0,m): 
+        for j in range(0,n): 
+            lbp_img[i, j] = local_bin_val(gray_scale, i, j) 
+
+    n_bins1 = int(lbp_img.max() + 1)
+    hist, _ = np.histogram(lbp_img, density=True, bins=n_bins1, range=(0, n_bins1))   #get the histogram for distance calculation task
+    return hist, lbp_img
 
