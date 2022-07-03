3ae359f0fb6219dacf7898b3e37c992d2ac2c7bf
ducanhho2296 <ducanh.ho2296@gmail.com>
2022-07-03 21:10:25 +0200

    update Arcface model

--- MFR_challenge/Face_recognition/arcface_model.py
+++ MFR_challenge/Face_recognition/arcface_model.py
@@ -65,3 +65,19 @@ def findCosineDistance(source_representation, test_representation):
     elif dist >= 0.35 and dist < 0.7: print("Must use LBP to verify this pair")
     else: print("not the same person")
     return dist
+
+#Create and load Arcface model for prediction
+def loadModel(model_path):
+	base_model = ResNet34()
+	inputs = base_model.inputs[0]
+	arcface_model = base_model.outputs[0]
+	arcface_model = layers.BatchNormalization(momentum=0.9, epsilon=2e-5)(arcface_model)
+	arcface_model = layers.Dropout(0.4)(arcface_model)
+	arcface_model = layers.Flatten()(arcface_model)
+	arcface_model = layers.Dense(512, activation=None, use_bias=True, kernel_initializer="glorot_normal")(arcface_model)
+	embedding = layers.BatchNormalization(momentum=0.9, epsilon=2e-5, name="embedding", scale=True)(arcface_model)
+	model = keras.models.Model(inputs, embedding, name=base_model.name)
+
+	model.load_weights(model_path)
+
+	return model
