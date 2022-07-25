ca4e58be89c11182ae4028a1aa356a9a6decbe12
Duc Anh Ho <ducanh.ho2296@gmail.com>
2022-07-25 14:54:47 +0200

    Update test.py

--- Masked Face Recognition with Arcface and LBP/test.py
+++ Masked Face Recognition with Arcface and LBP/test.py
@@ -8,3 +8,16 @@ from sklearn import metrics
 from sklearn.metrics import f1_score
 import matplotlib.pyplot as plt
 
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+parser = argparse.ArgumentParser(description="Run Arcface model validation.")
+parser.add_argument("--path", type=str, default="../", help="mfr implementation path")
+args = parser.parse_args()
+_path = args.path
+
+sys.path.append(_path)
+#---create a repository to store model.h5, create folder to store test-set
+# !mkdir model
+#!mkdir testsets
+#---download model from gg drive
+# import gdown
+# !gdown 1LVB3CdVejpmGHM28BpqqkbZP5hDEcdZY
