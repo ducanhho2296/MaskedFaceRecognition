e102c22bfd51e1139ec7249425134379c0d03ac0
Duc Anh Ho <ducanh.ho2296@gmail.com>
2022-07-26 15:03:21 +0200

    Update test.py

--- Masked Face Recognition with Arcface and LBP/test.py
+++ Masked Face Recognition with Arcface and LBP/test.py
@@ -117,3 +117,4 @@ y_pred = len([i for i, j in zip(flags, label_flags) if i == j])
 y_test = len(flags)
 acc = y_pred/y_test *100
 print("accuracy without LBP is: ", acc)
+print("F1 score without LBPis: ", f1_score(label_flags, flags))
