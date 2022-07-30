f293f972371072916b32fd9f49444b45a3c23837
Duc Anh Ho <ducanh.ho2296@gmail.com>
2022-07-30 10:17:24 +0200

    Update random_test.py

--- MFR with Arcface and LBP/random_test.py
+++ MFR with Arcface and LBP/random_test.py
@@ -5,3 +5,33 @@ import time
 
 random.seed(2468)
 
+for dirname in os.listdir(data_path):
+    genuine = [file for file in glob.glob(data_path + dirname + "/*")[0:6]]
+    random.shuffle(genuine)
+    imposter = random.sample([file for file in glob.glob(data_path + dirname + "/*")[6:]], 2)
+
+    sets = [[imposter[0]] + genuine[0:3], [imposter[1]] + genuine[3:]]
+
+    imposter = []
+    for set in sets:
+        start_pred = time.time()
+        # TODO add prediction here
+        time_per_prediction += time.time() - start_pred
+
+    # TODO determine index of imposter and add to imposter list
+
+    if imposter[0] == 0:
+        correct_predictions += 1
+    else:
+        bad_predictions.append(sets[0])
+    
+    if imposter[1] == 0:
+        correct_predictions += 1
+    else:
+        bad_predictions.append(sets[0])
+
+print("Bad predictions: ", bad_predictions)
+
+print("Accuracy: ", correct_predictions / (len(os.listdir(data_path)) * 2))
+print("Total time: {}s\tPrediction time: {}s\tMean prediction time per set: {}s".format(time.time() - start_total, 
+                                                                                        time_per_prediction, time_per_prediction / (len(os.listdir(data_path)) * 2)))
