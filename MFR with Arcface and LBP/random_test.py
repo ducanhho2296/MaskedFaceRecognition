83f6b0317bc811f031d6dc826f56d053d8374b39
Duc Anh Ho <ducanh.ho2296@gmail.com>
2022-07-30 10:17:32 +0200

    Update random_test.py

--- MFR with Arcface and LBP/random_test.py
+++ MFR with Arcface and LBP/random_test.py
@@ -7,6 +7,7 @@ random.seed(2468)
 
 data_path = "/content/drive/MyDrive/Colab Notebooks/comask20_testset"
 
+correct_predictions = 0
 for dirname in os.listdir(data_path):
     genuine = [file for file in glob.glob(data_path + dirname + "/*")[0:6]]
     random.shuffle(genuine)
