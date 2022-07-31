eae5a4643473453577eaf5b46075e85b56c2179f
Duc Anh Ho <ducanh.ho2296@gmail.com>
2022-07-31 11:45:29 +0200

    Update resnet_arcface.py

--- MFR with Arcface and LBP/resnet_arcface.py
+++ MFR with Arcface and LBP/resnet_arcface.py
@@ -66,7 +66,6 @@ def findCosineDistance(source_representation, test_representation):
     else: print("not the same person")
     return dist
 
-#Create and load Arcface model for prediction
 def loadModel(model_path):
 	base_model = ResNet34()
 	inputs = base_model.inputs[0]
