9fae6c841a5e2df97e1a7a4d8401cfc03309dce7
Duc Anh Ho <ducanh.ho2296@gmail.com>
2022-09-07 10:10:40 +0200

    Update resnet_arcface.py

--- MFR with Arcface and LBP/resnet_arcface.py
+++ MFR with Arcface and LBP/resnet_arcface.py
@@ -69,7 +69,6 @@ def findCosineDistance(source_representation, test_representation):
 def loadModel(model_path):
 	inputs = base_model.inputs[0]
 	arcface_model = base_model.outputs[0]
-	arcface_model = layers.BatchNormalization(momentum=0.9, epsilon=2e-5)(arcface_model)
 	arcface_model = layers.Dropout(0.4)(arcface_model)
 	arcface_model = layers.Flatten()(arcface_model)
 	arcface_model = layers.Dense(512, activation=None, use_bias=True, kernel_initializer="glorot_normal")(arcface_model)
